7a228cf666828697abeed96e1d5c1fbcc265f2d0
rateelbahathiq <rateelbahathiq@gmail.com>
2025-08-31 15:39:05 +0000

    add 3-print_numbers.c and update main.h and README.md

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9 followed by a new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
