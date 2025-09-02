70139d1d4d0d45376300e4cf9ad1de30e9058249
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-02 15:27:48 +0000

    add 9-fizz_buzz.c and update README.md

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 1 to 100, replacing multiples of
+ *        3 with "Fizz", multiples of 5 with "Buzz", and both with "FizzBuzz"
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+
+		if (num != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
--- more_functions_nested_loops/README.md
+++ more_functions_nested_loops/README.md
@@ -7,3 +7,4 @@
 6-print_line.c: Write a function that draws a straight line in the terminal.
 7-print_diagonal.c:Write a function that draws a diagonal line on the terminal.
 8-print_square.c: Write a function that prints a square, followed by a new line.
+9-fizz_buzz.c: Write a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.
