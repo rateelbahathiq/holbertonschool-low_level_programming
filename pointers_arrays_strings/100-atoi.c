1b2a3d01d56cfce6023a14a4779f7e4195081978
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-04 10:33:39 +0000

    add 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,54 +1,32 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
  * @s: pointer to the string
  *
- * Return: converted integer (0 if no digits)
+ * Return: the integer value of the string, or 0 if none
  */
 int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
-	int started = 0;
-	int res = 0;              /* build as NEGATIVE to avoid overflow */
-	int cutoff = INT_MIN / 10;
-	int limit = INT_MIN % 10; /* negative (e.g., -8) */
+	int num = 0;
+	int found_digit = 0;
 
+	/* skip non-digit characters, track sign */
 	while (s[i] != '\0')
 	{
-		if (!started && (s[i] == '+' || s[i] == '-'))
-		{
-			if (s[i] == '-')
-				sign = -sign;
-		}
+		if (s[i] == '-')
+			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			int d = s[i] - '0';
-
-			started = 1;
-
-			/* Check overflow for: res = res * 10 - d */
-			if (res < cutoff || (res == cutoff && -d < limit))
-				return (sign == -1 ? INT_MIN : INT_MAX);
-
-			res = res * 10 - d;
+			found_digit = 1;
+			num = num * 10 + (s[i] - '0');
 		}
-		else if (started)
-		{
+		else if (found_digit)
 			break;
-		}
 		i++;
 	}
 
-	/* If no digits were found, res remains 0 */
-	if (sign == -1)
-		return (res); /* already negative */
-
-	/* Negating INT_MIN would overflow; clamp just in case */
-	if (res == INT_MIN)
-		return (INT_MAX);
-
-	return (-res);
+	return (sign * num);
 }
