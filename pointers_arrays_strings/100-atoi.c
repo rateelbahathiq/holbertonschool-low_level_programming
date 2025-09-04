840bb6ba99767fa66e4eb250247610b9004e2923
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-04 07:28:03 +0000

    add task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,25 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int num = 0;
-	int found_digit = 0;
+        int i = 0;
+        int sign = 1;
+        int num = 0;
+        int found_digit = 0;
 
-	/* skip non-digit characters, track sign */
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			found_digit = 1;
-			num = num * 10 + (s[i] - '0');
-		}
-		else if (found_digit)
-			break;
-		i++;
-	}
+        /* skip non-digit characters, track sign */
+        while (s[i] != '\0')
+        {
+                if (s[i] == '-')
+                        sign *= -1;
+                else if (s[i] >= '0' && s[i] <= '9')
+                {
+                        found_digit = 1;
+                        num = num * 10 + (s[i] - '0');
+                }
+                else if (found_digit)
+                        break;
+                i++;
+        }
 
-	return (sign * num);
+        return (sign * num);
 }
