0eca2d133a93020ce951a161c0b9e27667eaeeea
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-04 07:57:23 +0000

    Add overflow-safe _atoi function (Task 100)

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -5,16 +5,16 @@
  * _atoi - converts a string to an integer
  * @s: pointer to the string
  *
- * Return: converted integer value, or 0 if no digits are found
+ * Return: converted integer (0 if no digits)
  */
 int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
 	int started = 0;
-	int res = 0;            /* accumulate as NEGATIVE to avoid overflow */
+	int res = 0;              /* build as NEGATIVE to avoid overflow */
 	int cutoff = INT_MIN / 10;
-	int limit = INT_MIN % 10;   /* negative (e.g., -8) */
+	int limit = INT_MIN % 10; /* negative (e.g., -8) */
 
 	while (s[i] != '\0')
 	{
@@ -29,11 +29,9 @@ int _atoi(char *s)
 
 			started = 1;
 
-			/* Check would-be overflow for: res = res * 10 - d */
+			/* Check overflow for: res = res * 10 - d */
 			if (res < cutoff || (res == cutoff && -d < limit))
-			{
-				return (sign == -1) ? INT_MIN : INT_MAX;
-			}
+				return (sign == -1 ? INT_MIN : INT_MAX);
 
 			res = res * 10 - d;
 		}
@@ -44,10 +42,12 @@ int _atoi(char *s)
 		i++;
 	}
 
+	/* If no digits were found, res remains 0 */
 	if (sign == -1)
-		return (res);        /* already negative */
+		return (res); /* already negative */
 
-	if (res == INT_MIN)      /* negating INT_MIN would overflow */
+	/* Negating INT_MIN would overflow; clamp just in case */
+	if (res == INT_MIN)
 		return (INT_MAX);
 
 	return (-res);
