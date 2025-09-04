292a42a17cd3c4380c633f0f163341479dfcda68
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-04 07:42:16 +0000

    Add overflow-safe _atoi function (Task 100)

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,54 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
  * @s: pointer to the string
  *
- * Return: the integer value of the string, or 0 if none
+ * Return: converted integer value, or 0 if no digits are found
  */
 int _atoi(char *s)
 {
-        int i = 0;
-        int sign = 1;
-        int num = 0;
-        int found_digit = 0;
+	int i = 0;
+	int sign = 1;
+	int started = 0;
+	int res = 0;            /* accumulate as NEGATIVE to avoid overflow */
+	int cutoff = INT_MIN / 10;
+	int limit = INT_MIN % 10;   /* negative (e.g., -8) */
 
-        /* skip non-digit characters, track sign */
-        while (s[i] != '\0')
-        {
-                if (s[i] == '-')
-                        sign *= -1;
-                else if (s[i] >= '0' && s[i] <= '9')
-                {
-                        found_digit = 1;
-                        num = num * 10 + (s[i] - '0');
-                }
-                else if (found_digit)
-                        break;
-                i++;
-        }
+	while (s[i] != '\0')
+	{
+		if (!started && (s[i] == '+' || s[i] == '-'))
+		{
+			if (s[i] == '-')
+				sign = -sign;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			int d = s[i] - '0';
 
-        return (sign * num);
+			started = 1;
+
+			/* Check would-be overflow for: res = res * 10 - d */
+			if (res < cutoff || (res == cutoff && -d < limit))
+			{
+				return (sign == -1) ? INT_MIN : INT_MAX;
+			}
+
+			res = res * 10 - d;
+		}
+		else if (started)
+		{
+			break;
+		}
+		i++;
+	}
+
+	if (sign == -1)
+		return (res);        /* already negative */
+
+	if (res == INT_MIN)      /* negating INT_MIN would overflow */
+		return (INT_MAX);
+
+	return (-res);
 }
