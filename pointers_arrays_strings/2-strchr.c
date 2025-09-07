8ed04165660cbe857c86810d9f2ed3e35836c548
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-07 14:30:41 +0000

    add 2-strchr.c and update main.h and README.md

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to find
+ *
+ * Return: pointer to the first occurrence of c in s,
+ * or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (*s == c) /* check the null terminator as well */
+		return (s);
+
+	return (NULL);
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -19,3 +19,4 @@
 7.leet.c:Write a function that encodes a string into 1337.
 0-memset.c:Write a function that fills memory with a constant byte.
 1-memcpy.c:Write a function that copies memory area.
+2-strchr.c:Write a function that locates a character in a string.
