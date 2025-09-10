35ee7c9d55e8f728e0e330309f144e57b02284be
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-10 15:45:09 +0000

    add 2-args.c and update README.md and main.h

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments received
+ * @argc: Argument count
+ * @argv: Argument vector
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
--- argc_argv/README.md
+++ argc_argv/README.md
@@ -1,2 +1,3 @@
 0-whatsmyname.c:Write a program that prints its name, followed by a new line.
 1-args.c:Write a program that prints the number of arguments passed into it.
+2-args.c:Write a program that prints all arguments it receives.
