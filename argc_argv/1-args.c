f4dfe81f2f8fa301a290cbbe342a770ff7c798c8
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-10 15:13:40 +0000

    update 1-args.c file

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,15 +2,14 @@
 
 /**
  * main - Prints the number of arguments passed into the program
- * @argc: Argumant count
+ * @argc: Argument count
  * @argv: Argument vector (unused)
  *
  * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	printf("%d\n", argc-1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
