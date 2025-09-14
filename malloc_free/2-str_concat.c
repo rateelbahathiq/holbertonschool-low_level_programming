f3f66388e70c5aa224d9c87f12a622e26c4dec30
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-14 08:29:04 +0000

    add 2-str_concat.c and update README.md and main.h

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings into a newly allocated memory
+ * @s1: first string (may be NULL)
+ * @s2: second string (may be NULL)
+ *
+ * Return: pointer to the new concatenated string,
+ *         or NULL if memory allocation fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *res;
+	unsigned int i, j, len1 = 0, len2 = 0;
+
+	if (s1 != NULL)
+	{
+		while (s1[len1] != '\0')
+			len1++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[len2] != '\0')
+			len2++;
+	}
+
+	res = malloc((len1 + len2 + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		res[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+		res[i + j] = s2[j];
+
+	res[len1 + len2] = '\0';
+
+	return (res);
+}
