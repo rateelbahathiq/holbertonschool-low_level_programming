f946bee941391c38212d11456ff0e44f128a075e
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-20 16:33:03 +0000

    add 2-calloc.c and update main.h and README.md

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array and sets it to zero
+ * @nmemb: Number of elements
+ * @size: Size of each element
+ *
+ * Return: Pointer to allocated memory, or NULL if nmemb or size is 0
+ *         or if malloc fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -1,2 +1,3 @@
 -0-malloc_checked.:Write a function that allocates memory using malloc.
 -1-1-string_nconcat.c:Write a function that concatenates two strings.
+-2-calloc.c:Write a function that allocates memory for an array, using malloc
