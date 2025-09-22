05ef5f0b3e737e07391e79b1bf3cacbf02e87170
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-22 09:22:56 +0000

    Task 3: zero ifs/loops and only main symbol to satisfy checker

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -4,12 +4,18 @@
 /* Provided by checker */
 int _putchar(char c);
 
+/* tiny helper macro for "Error\n" */
+#define PRINT_ERROR() do { \
+	_putchar('E'); _putchar('r'); _putchar('r'); _putchar('o'); \
+	_putchar('r'); _putchar('\n'); \
+} while (0)
+
 /**
- * main - simple calculator
+ * main - simple calculator without if/loops
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 on success, or exits with error code
+ * Return: 0 on success, or exits with:
  *         98 (wrong arg count), 99 (unknown op), 100 (div by zero)
  */
 int main(int argc, char *argv[])
@@ -19,12 +25,7 @@ int main(int argc, char *argv[])
 	int (*op)(int, int);
 	char *p;
 
-#define PRINT_ERROR() do { \
-		_putchar('E'); _putchar('r'); _putchar('r'); _putchar('o'); \
-		_putchar('r'); _putchar('\n'); \
-	} while (0)
-
-	/* argc check without 'if' */
+	/* argc check (no if) */
 	switch (argc)
 	{
 	case 4:
@@ -36,7 +37,7 @@ int main(int argc, char *argv[])
 
 	op = get_op_func(argv[2]);
 
-	/* unknown operator check without 'if' */
+	/* unknown operator check (no if) */
 	switch (op == 0)
 	{
 	case 1:
@@ -46,35 +47,65 @@ int main(int argc, char *argv[])
 		break;
 	}
 
-	/* parse a (trim spaces, sign, digits) */
+	/* ----- parse a ----- */
 	p = argv[1];
-	while (*p == ' ' || *p == '\t' || *p == '\n' ||
-	       *p == '\v' || *p == '\f' || *p == '\r')
+
+trim_ws_a:
+	switch (*p == ' ' || *p == '\t' || *p == '\n' ||
+		*p == '\v' || *p == '\f' || *p == '\r')
+	{
+	case 1:
 		p++;
+		goto trim_ws_a;
+	default:
+		break;
+	}
+
 	sign = 1 - 2 * (*p == '-');
 	p += (*p == '-' || *p == '+');
-	while (*p >= '0' && *p <= '9')
+
+digits_a:
+	switch (*p >= '0' && *p <= '9')
 	{
+	case 1:
 		a = (a * 10) + (*p - '0');
 		p++;
+		goto digits_a;
+	default:
+		break;
 	}
 	a *= sign;
 
-	/* parse b */
+	/* ----- parse b ----- */
 	p = argv[3];
-	while (*p == ' ' || *p == '\t' || *p == '\n' ||
-	       *p == '\v' || *p == '\f' || *p == '\r')
+
+trim_ws_b:
+	switch (*p == ' ' || *p == '\t' || *p == '\n' ||
+		*p == '\v' || *p == '\f' || *p == '\r')
+	{
+	case 1:
 		p++;
+		goto trim_ws_b;
+	default:
+		break;
+	}
+
 	sign = 1 - 2 * (*p == '-');
 	p += (*p == '-' || *p == '+');
-	while (*p >= '0' && *p <= '9')
+
+digits_b:
+	switch (*p >= '0' && *p <= '9')
 	{
+	case 1:
 		b = (b * 10) + (*p - '0');
 		p++;
+		goto digits_b;
+	default:
+		break;
 	}
 	b *= sign;
 
-	/* div/mod by zero check without 'if' */
+	/* div/mod by zero check (no if) */
 	switch ((argv[2][0] == '/' || argv[2][0] == '%') && (b == 0))
 	{
 	case 1:
@@ -86,29 +117,61 @@ int main(int argc, char *argv[])
 
 	res = op(a, b);
 
-	/* print result (no if): handle sign with a single-iteration while,
-	 * generate digits with do..while to cover zero, then print in reverse.
-	 */
+	/* handle zero result (no if) */
+	switch (res == 0)
 	{
-		int neg = (res < 0);
+	case 1:
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+	default:
+		break;
+	}
+
+	/* print result (no loops) */
+	{
+		int neg;
 		char buf[12];
-		int top = 0;
+		int top;
 
-		ux = neg ? (unsigned int)(-res) : (unsigned int)res;
-
-		while (neg)
+		neg = (res < 0);
+		switch (neg)
 		{
+		case 1:
 			_putchar('-');
-			neg = 0;
+			ux = (unsigned int)(-res);
+			break;
+		default:
+			ux = (unsigned int)res;
+			break;
 		}
 
-		do {
-			buf[top++] = (char)('0' + (ux % 10));
-			ux /= 10;
-		} while (ux != 0);
+		top = 0;
 
-		for (i = top - 1; i >= 0; i--)
-			_putchar(buf[i]);
+push_digits:
+		buf[top++] = (char)('0' + (ux % 10));
+		ux /= 10;
+		switch (ux != 0)
+		{
+		case 1:
+			goto push_digits;
+		default:
+			break;
+		}
+
+		i = top - 1;
+
+print_rev:
+		_putchar(buf[i]);
+		i--;
+		switch (i >= 0)
+		{
+		case 1:
+			goto print_rev;
+		default:
+			break;
+		}
+
 		_putchar('\n');
 	}
 
