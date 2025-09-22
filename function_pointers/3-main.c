b1b089a46887ed142fc2813d22b151b491f7bbe2
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-22 09:05:05 +0000

    Task 3: Inline helpers so 3-main.o defines only main (fix checker)

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,84 +1,8 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
-/* Provided by checker; declared in function_pointers project */
+/* Provided by checker */
 int _putchar(char c);
-
-/**
- * print_str - prints a C-string using _putchar
- * @s: string to print
- *
- * Return: Nothing.
- */
-static void print_str(const char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-}
-
-/**
- * _atoi_simple - converts a string to int (handles optional +/- and digits)
- * @s: string to convert
- *
- * Return: converted integer (undefined on overflow, as usual for simple atoi)
- */
-static int _atoi_simple(char *s)
-{
-	int sign = 1;
-	int num = 0;
-
-	while (*s == ' ' || *s == '\t' || *s == '\n' ||
-	       *s == '\v' || *s == '\f' || *s == '\r')
-		s++;
-
-	if (*s == '-' || *s == '+')
-	{
-		if (*s == '-')
-			sign = -1;
-		s++;
-	}
-
-	while (*s >= '0' && *s <= '9')
-	{
-		num = (num * 10) + (*s - '0');
-		s++;
-	}
-
-	return (sign * num);
-}
-
-/**
- * print_int - prints an integer using _putchar
- * @n: integer to print
- *
- * Return: Nothing.
- */
-static void print_int(int n)
-{
-	unsigned int x;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		x = (unsigned int)(-n);
-	}
-	else
-	{
-		x = (unsigned int)n;
-	}
-
-	if (x / 10)
-		print_int((int)(x / 10));
-	_putchar((x % 10) + '0');
-}
 
 /**
  * main - simple calculator
@@ -86,37 +10,110 @@ static void print_int(int n)
  * @argv: argument vector
  *
  * Return: 0 on success, or exits with error code
+ *         98 (wrong arg count), 99 (unknown op), 100 (div by zero)
  */
 int main(int argc, char *argv[])
 {
-	int a, b, res;
+	int a = 0, b = 0, res, sign, i;
+	unsigned int ux;
 	int (*op)(int, int);
+	char *p;
+
+	/* print "Error\n" helper (inline) */
+#define PRINT_ERROR() do { \
+		_putchar('E'); _putchar('r'); _putchar('r'); _putchar('o'); \
+		_putchar('r'); _putchar('\n'); \
+	} while (0)
 
 	if (argc != 4)
 	{
-		print_str("Error\n");
+		PRINT_ERROR();
 		exit(98);
 	}
 
 	op = get_op_func(argv[2]);
 	if (op == 0)
 	{
-		print_str("Error\n");
+		PRINT_ERROR();
 		exit(99);
 	}
 
-	a = _atoi_simple(argv[1]);
-	b = _atoi_simple(argv[3]);
+	/* atoi for argv[1] (inline, no extra function symbols) */
+	p = argv[1];
+	while (*p == ' ' || *p == '\t' || *p == '\n' ||
+	       *p == '\v' || *p == '\f' || *p == '\r')
+		p++;
+	sign = 1;
+	if (*p == '-' || *p == '+')
+	{
+		if (*p == '-')
+			sign = -1;
+		p++;
+	}
+	while (*p >= '0' && *p <= '9')
+	{
+		a = (a * 10) + (*p - '0');
+		p++;
+	}
+	a *= sign;
+
+	/* atoi for argv[3] */
+	p = argv[3];
+	while (*p == ' ' || *p == '\t' || *p == '\n' ||
+	       *p == '\v' || *p == '\f' || *p == '\r')
+		p++;
+	sign = 1;
+	if (*p == '-' || *p == '+')
+	{
+		if (*p == '-')
+			sign = -1;
+		p++;
+	}
+	while (*p >= '0' && *p <= '9')
+	{
+		b = (b * 10) + (*p - '0');
+		p++;
+	}
+	b *= sign;
 
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 	{
-		print_str("Error\n");
+		PRINT_ERROR();
 		exit(100);
 	}
 
 	res = op(a, b);
-	print_int(res);
-	_putchar('\n');
+
+	/* print integer result (inline, no helper symbol) */
+	if (res == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+	}
+	if (res < 0)
+	{
+		_putchar('-');
+		ux = (unsigned int)(-res);
+	}
+	else
+	{
+		ux = (unsigned int)res;
+	}
+
+	{
+		char buf[12];
+		int top = 0;
+
+		while (ux != 0)
+		{
+			buf[top++] = (char)('0' + (ux % 10));
+			ux /= 10;
+		}
+		for (i = top - 1; i >= 0; i--)
+			_putchar(buf[i]);
+		_putchar('\n');
+	}
 
 	return (0);
 }
