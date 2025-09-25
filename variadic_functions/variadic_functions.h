8ec9ae9c2c7ff981014919d4b9402bb3f71a73d8
rateelbahathiq <rateelbahathiq@gmail.com>
2025-09-25 07:02:58 +0000

    add 0-sum_them_all.c and variadic_functions.h and README.md

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/* Provided by checker; do not push your own _putchar.c */
+int _putchar(char c);
+
+/* Prototypes */
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
