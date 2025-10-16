f12cc51144db75a1430ea20446cf6ced391f8dae
rateelbahathiq <rateelbahathiq@gmail.com>
2025-10-16 08:27:05 +0000

    fix betty style

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -7,19 +7,19 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc’ed string)
+ * @str: string (malloc'ed)
  * @len: length of the string
- * @next: points to the next node
+ * @next: pointer to the next node
  *
  * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 
-#endif
+#endif /* LISTS_H */
