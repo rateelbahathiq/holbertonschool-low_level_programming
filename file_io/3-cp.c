6f36e4bb04d3473eaf5863172adfb8f45641e4c0
rateelbahathiq <rateelbahathiq@gmail.com>
2025-10-18 13:48:40 +0000

    fix 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,14 +1,9 @@
 #include "main.h"
 
-/**
- * error_exit - prints an error message and exits with a given code
- * @code: exit status
- * @msg: error message format string
- * @arg: argument for the message
- */
-void error_exit(int code, const char *msg, const char *arg)
+/* Print error to stderr and exit with code */
+static void die(int code, const char *fmt, const char *arg)
 {
-	dprintf(STDERR_FILENO, msg, arg);
+	dprintf(STDERR_FILENO, fmt, arg);
 	exit(code);
 }
 
@@ -17,11 +12,12 @@ void error_exit(int code, const char *msg, const char *arg)
  * @ac: argument count
  * @av: argument vector
  *
- * Return: 0 on success
+ * Return: 0 on success, exits with codes per spec otherwise
  */
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, r, w;
+	int fd_from, fd_to;
+	ssize_t r, w, off;
 	char buf[1024];
 
 	if (ac != 3)
@@ -32,33 +28,50 @@ int main(int ac, char **av)
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+		die(98, "Error: Can't read from file %s\n", av[1]);
 
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		error_exit(99, "Error: Can't write to %s\n", av[2]);
-
-	while ((r = read(fd_from, buf, 1024)) > 0)
 	{
-		w = write(fd_to, buf, r);
-		if (w == -1 || w != r)
-			error_exit(99, "Error: Can't write to %s\n", av[2]);
+		if (close(fd_from) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+		die(99, "Error: Can't write to %s\n", av[2]);
 	}
 
-	if (r == -1)
-		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+	for (;;)
+	{
+		r = read(fd_from, buf, sizeof(buf));
+		if (r == 0) /* EOF */
+			break;
+		if (r == -1) /* READ ERROR -> 98 */
+		{
+			if (close(fd_from) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+			if (close(fd_to) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+			die(98, "Error: Can't read from file %s\n", av[1]);
+		}
+
+		off = 0;
+		while (off < r) /* ensure full write, else -> 99 */
+		{
+			w = write(fd_to, buf + off, r - off);
+			if (w == -1)
+			{
+				if (close(fd_from) == -1)
+					dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+				if (close(fd_to) == -1)
+					dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+				die(99, "Error: Can't write to %s\n", av[2]);
+			}
+			off += w;
+		}
+	}
 
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 
 	return (0);
 }
