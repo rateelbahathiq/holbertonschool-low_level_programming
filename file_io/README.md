edef3a13057d7ad3ac84d4ec049bc54870d94fc7
rateelbahathiq <rateelbahathiq@gmail.com>
2025-10-18 13:26:11 +0000

    add task 1

--- file_io/README.md
+++ file_io/README.md
@@ -1,1 +1,2 @@
 task0: Write a function that reads a text file and prints it to the POSIX standard output.
+task1: Create a function that creates a file.
--- file_io/main.h
+++ file_io/main.h
@@ -6,8 +6,8 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
