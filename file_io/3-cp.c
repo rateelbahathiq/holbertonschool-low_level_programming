e072f71e18ea7f4f418afbb75159e62f6c7aeb80
rateelbahathiq <rateelbahathiq@gmail.com>
2025-10-18 13:43:29 +0000

    add task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * error_exit - prints an error message and exits with a given code
+ * @code: exit status
+ * @msg: error message format string
+ * @arg: argument for the message
+ */
+void error_exit(int code, const char *msg, const char *arg)
+{
+	dprintf(STDERR_FILENO, msg, arg);
+	exit(code);
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to, r, w;
+	char buf[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_exit(99, "Error: Can't write to %s\n", av[2]);
+
+	while ((r = read(fd_from, buf, 1024)) > 0)
+	{
+		w = write(fd_to, buf, r);
+		if (w == -1 || w != r)
+			error_exit(99, "Error: Can't write to %s\n", av[2]);
+	}
+
+	if (r == -1)
+		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
--- file_io/README.md
+++ file_io/README.md
@@ -1,3 +1,4 @@
 task0: Write a function that reads a text file and prints it to the POSIX standard output.
 task1: Create a function that creates a file.
 task2: Write a function that appends text at the end of a file.
+task3: Write a program that copies the content of a file to another file.
