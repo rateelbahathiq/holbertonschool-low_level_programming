cd5e61496bc14db6719cf2b18ceb60159c2bd6c5
rateelbahathiq <rateelbahathiq@gmail.com>
2025-10-29 08:34:26 +0000

    fix betty errors

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,28 +8,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
-    unsigned long int i;
+	hash_table_t *ht;
+	unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-    /* Set the size */
-    ht->size = size;
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
 
-    /* Allocate memory for the array of node pointers */
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-    if (ht->array == NULL)
-    {
-        free(ht);
-        return (NULL);
-    }
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-    /* Initialize all array elements to NULL */
-    for (i = 0; i < size; i++)
-        ht->array[i] = NULL;
-
-    return (ht);
+	return (ht);
 }
